3a81973e16ceb0132421e5cf85b3fa90eab946df
Matheus <matheusrodrigofilho@outlook.com>
2020-05-13 20:44:12 -0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Button, Alert, ImageBackground} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import axios from 'axios';
+
+const image = {uri: "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"};
+
+export default class App extends Component {
+
+
+  
+  getDataUsingGet(){
+    axios.get('https://api.github.com/repos/facebook/react')
+    .then(function(response){
+      alert("Foo1 = "+response.data.id +"\nFoo2 = "+ response.data.node_id); 
+      
+
+      
+    }); 
+  }
+
+  
+
+ 
+  render() {
+    return (
+      <ImageBackground source={image} style={styles.image}>
+        <Button style={styles.Button} title='Botão' onPress={this.getDataUsingGet}/>
+      </ImageBackground>
+    );
+  }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  Button :{
     justifyContent: 'center',
+    flex:1,
+
   },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  
 });
