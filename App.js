1544c0b05619bb92fc44325c5044d0144f027f70
Matheus <matheusrodrigofilho@outlook.com>
2020-05-13 21:54:02 -0300

    Estilizacao do button

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Button, Alert, ImageBackground} from 'react-native';
+import { StyleSheet, Text, ImageBackground, TouchableOpacity, View} from 'react-native';
 
 import axios from 'axios';
 
-const image = {uri: "https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"};
-
 export default class App extends Component {
 
-
-  
-  getDataUsingGet(){
+  getData(){
     axios.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
     .then(function(response){
       alert("Foo1 = "+response.data.foo1 +"\nFoo2 = "+ response.data.foo2); 
@@ -18,23 +14,35 @@ export default class App extends Component {
  
   render() {
     return (
-      <ImageBackground source={image} style={styles.image}>
-        <Button style={styles.Button} title='Botão' onPress={this.getDataUsingGet}/>
+      <ImageBackground source={require('./assets/fundo.jpeg')} style={styles.image}>
+        
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.getData()}
+            >
+              <Text style={styles.text}>Botão</Text>
+          </TouchableOpacity>
+      
       </ImageBackground>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
-  Button :{
-    justifyContent: 'center',
-    flex:1,
-
+  button:{
+    alignItems:'center', 
+    borderRadius:20,
   },
   image: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent:'center',
   },
-  
+
+  text:{
+    backgroundColor: 'gray',
+    fontSize:30,
+    padding: 25,
+    borderColor: 'black',
+    borderRadius:25 
+  }
 });
