8c5449c1ebc7d52dd2a6d83fde1e3b6bbb8a661d
Matheus <matheusrodrigofilho@outlook.com>
2020-05-14 12:19:16 -0300

    Organização do componente

--- App.js
+++ App.js
@@ -1,48 +1,13 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, ImageBackground, TouchableOpacity, View} from 'react-native';
 
-import axios from 'axios';
-
+import Index from './page';
 export default class App extends Component {
 
-  getData(){
-    axios.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
-    .then(function(response){
-      alert("Foo1 = "+response.data.foo1 +"\nFoo2 = "+ response.data.foo2); 
-    }); 
-  }
+  
  
   render() {
     return (
-      <ImageBackground source={require('./assets/fundo.jpeg')} style={styles.image}>
-        
-          <TouchableOpacity
-            style={styles.button}
-            onPress={this.getData()}
-            >
-              <Text style={styles.text}>Botão</Text>
-          </TouchableOpacity>
-      
-      </ImageBackground>
+      <Index />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  button:{
-    alignItems:'center', 
-    borderRadius:20,
-  },
-  image: {
-    flex: 1,
-    justifyContent:'center',
-  },
-
-  text:{
-    backgroundColor: 'gray',
-    fontSize:30,
-    padding: 25,
-    borderColor: 'black',
-    borderRadius:25 
-  }
-});
---
+++ page/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, ImageBackground, TouchableOpacity, View} from 'react-native';
+
+import axios from 'axios';
+
+export default class Index extends Component {
+
+  getData(){
+    axios.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
+    .then(function(response){
+      alert("Foo1 = "+response.data.foo1 +"\nFoo2 = "+ response.data.foo2); 
+    }); 
+  }
+ 
+  render() {
+    return (
+      <ImageBackground source={require('../assets/fundo.jpeg')} style={styles.image}>
+        
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.getData()}
+            >
+              <Text style={styles.text}>Botão</Text>
+          </TouchableOpacity>
+      
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button:{
+    alignItems:'center', 
+    borderRadius:20,
+  },
+  image: {
+    flex: 1,
+    justifyContent:'center',
+  },
+
+  text:{
+    backgroundColor: 'gray',
+    fontSize:30,
+    padding: 25,
+    borderColor: 'black',
+    borderRadius:25 
+  }
+});
